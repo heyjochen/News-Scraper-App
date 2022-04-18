533fbe3f1ed90a4396cb6f64fbb312f0c117c215
heyjochen <jochen@stierberger.com>
2022-04-18 10:34:30 -0500

    add functions and news-links

--- js/main.js
+++ js/main.js
@@ -3,16 +3,67 @@
 // Hide API Key
 // change whole API
 
-
-// Import the API Key
-// import WEATHER_API_KEY from './apikey.js';
-
 class News{
   //What to put in here?
   constructor(){
   }
+  
 
   getNewsArticles(category){
+    // Checks if there already is content, if so it removes it
+    const newsSection = document.getElementById("news")
+    while (newsSection.firstChild) {
+        newsSection.firstChild.remove()
+    } 
+
+    fetch(
+      `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=${category}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-api-key': 'NbV-VXpvd17m4kTWIYsJN0ofMU326FM_QJwSCwLiR2I'
+        }
+      }
+    )
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+     data.articles.forEach((e) => {
+
+       // Create an populate headings
+       const heading = document.createElement('h1');
+       // Slice the headline title up until '-'
+      //  const indexOfDash = e.title.indexOf('-')
+      //  const headingModified = e.title.slice(0, indexOfDash-1)
+       heading.className = 'news-title';
+       heading.textContent = e.title;
+       document.querySelector('#news').appendChild(heading);
+
+       // Create an populate sources
+       const source = document.createElement('h2');
+       source.className = 'news-source';
+       source.textContent = e.clean_url;
+       document.querySelector('#news').appendChild(source);
+
+       //Link the Sources with the
+       const link = document.createElement('a');
+       link.className = 'news-link';
+       link.textContent = 'Read More';
+       link.href = e.link
+       document.querySelector('#news').appendChild(link);
+
+       // Add the divider
+      const divider = document.createElement('div');
+      divider.className = 'news-divider';
+      document.querySelector('#news').appendChild(divider);
+     })
+
+
+    })
+    .catch(err => {
+    console.log(`error ${err}`)
+    });
+
+    /*
     // Checks if there already is content, if so it removes it
     const newsSection = document.getElementById("news")
     while (newsSection.firstChild) {
@@ -57,8 +108,10 @@ class News{
     .catch(err => {
     console.log(`error ${err}`)
     });
-  }
 
+    */
+  }
+  
 }
 
 //Create News
@@ -66,18 +119,32 @@ const newHeadlines = new News()
 
 // Buttons
 const BUSINESS = document.querySelector('#business')
-const ENTERTAINMENT = document.querySelector('#entertainment')
-const GENERAL = document.querySelector('#general')
-const HEALTH = document.querySelector('#health')
+const BEAUTY = document.querySelector('#beauty')
+const ECONOMICS = document.querySelector('#economics')
+const ENERGY = document.querySelector('#energy')
+const FINANCE = document.querySelector('#finance')
+const FOOD = document.querySelector('#food')
+const GAMING = document.querySelector('#gaming')
+const MUSIC = document.querySelector('#music')
 const SCIENCE = document.querySelector('#science')
-const SPORTS = document.querySelector('#sports')
+const SPORT = document.querySelector('#sport')
 const TECHNOLOGY = document.querySelector('#technology')
+const TRAVEL = document.querySelector('#travel')
+const POLITICS = document.querySelector('#politics')
+const WORLD = document.querySelector('#world')
 
 // Click-Events
 BUSINESS.addEventListener('click', () => newHeadlines.getNewsArticles('business'))
-ENTERTAINMENT.addEventListener('click', () => newHeadlines.getNewsArticles('entertainment'))
-GENERAL.addEventListener('click', () => newHeadlines.getNewsArticles('general'))
-HEALTH.addEventListener('click', () => newHeadlines.getNewsArticles('health'))
+BEAUTY.addEventListener('click', () => newHeadlines.getNewsArticles('beauty'))
+ECONOMICS.addEventListener('click', () => newHeadlines.getNewsArticles('economics'))
+ENERGY.addEventListener('click', () => newHeadlines.getNewsArticles('energy'))
+FINANCE.addEventListener('click', () => newHeadlines.getNewsArticles('finance'))
+FOOD.addEventListener('click', () => newHeadlines.getNewsArticles('food'))
+GAMING.addEventListener('click', () => newHeadlines.getNewsArticles('gaming'))
+MUSIC.addEventListener('click', () => newHeadlines.getNewsArticles('music'))
 SCIENCE.addEventListener('click', () => newHeadlines.getNewsArticles('science'))
-SPORTS.addEventListener('click', () => newHeadlines.getNewsArticles('sports'))
-TECHNOLOGY.addEventListener('click', () => newHeadlines.getNewsArticles('health'))
+SPORT.addEventListener('click', () => newHeadlines.getNewsArticles('sport'))
+TECHNOLOGY.addEventListener('click', () => newHeadlines.getNewsArticles('tech'))
+TRAVEL.addEventListener('click', () => newHeadlines.getNewsArticles('travel'))
+POLITICS.addEventListener('click', () => newHeadlines.getNewsArticles('politics'))
+WORLD.addEventListener('click', () => newHeadlines.getNewsArticles('world'))
