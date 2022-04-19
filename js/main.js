9d50e832038c7ab878cf6da073209f5f6d1a9748
heyjochen <jochen@stierberger.com>
2022-04-19 09:53:11 -0500

    change style.css

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ class News{
     .then(data => {
      data.articles.forEach((e,i) => {
 
+
       // Create article element for each article
       const article = document.createElement('article')
       article.id = `article${i}`
@@ -36,23 +37,18 @@ class News{
       
       // Create and populate headings
        const heading = document.createElement('a');
-       heading.className = 'news-title';
+       heading.className = 'heading1-news-title';
        heading.textContent = e.title;
        heading.href = e.link
        document.querySelector(`#article${i}`).appendChild(heading);
 
+
        // Create and populate sources
        const source = document.createElement('h2');
-       source.className = 'news-source';
+       source.className = 'heading2-news-source';
        source.textContent = e.clean_url;
        document.querySelector(`#article${i}`).appendChild(source);
 
-       //Link the Sources with the
-      //  const link = document.createElement('a');
-      //  link.className = 'news-link';
-      //  link.textContent = 'Read More';
-      //  link.href = e.link
-      //  document.querySelector(`.article${i}`).appendChild(link);
 
       // Add the divider
       const divider = document.createElement('div');
@@ -66,59 +62,13 @@ class News{
     console.log(`error ${err}`)
     });
 
-    /*
-    // Checks if there already is content, if so it removes it
-    const newsSection = document.getElementById("news")
-    while (newsSection.firstChild) {
-        newsSection.firstChild.remove()
-    } 
-
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=fe0f8edc63764895be223c86d367cf91`)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-     data.articles.forEach((e) => {
-
-       // Create an populate headings
-       const heading = document.createElement('h1');
-       // Slice the headline title up until '-'
-       const indexOfDash = e.title.indexOf('-')
-       const headingModified = e.title.slice(0, indexOfDash-1)
-       heading.className = 'news-title';
-       heading.textContent = headingModified;
-       document.querySelector('#news').appendChild(heading);
-
-       // Create an populate sources
-       const source = document.createElement('h2');
-       source.className = 'news-source';
-       source.textContent = e.source.name;
-       document.querySelector('#news').appendChild(source);
-
-       //Link the Sources with the
-       const link = document.createElement('a');
-       link.className = 'news-link';
-       link.textContent = 'Read More';
-       link.href = e.url
-       document.querySelector('#news').appendChild(link);
-
-       // Add the divider
-      const divider = document.createElement('div');
-      divider.className = 'news-divider';
-      document.querySelector('#news').appendChild(divider);
-     })
-
-
-    })
-    .catch(err => {
-    console.log(`error ${err}`)
-    });
-
-    */
   }
   
 }
 
 //Create News
 const newHeadlines = new News()
+
 
 // Buttons
 const BUSINESS = document.querySelector('#business')
@@ -135,6 +85,7 @@ const TECHNOLOGY = document.querySelector('#technology')
 const TRAVEL = document.querySelector('#travel')
 const POLITICS = document.querySelector('#politics')
 const WORLD = document.querySelector('#world')
+
 
 // Click-Events
 BUSINESS.addEventListener('click', () => newHeadlines.getNewsArticles('business'))
