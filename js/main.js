1e52b3441e4dcb5343a4e09d72dd02ef251ed0d7
heyjochen <jochen@stierberger.com>
2022-05-21 10:31:50 -0500

    update code structure

--- js/main.js
+++ js/main.js
@@ -1,109 +1,89 @@
-// To Do
-// fix space between Twitter Link and Logo
-// make responsive
-// check if following article is same as previous, if so don't display
-// fetch in function outside of the class
+import { header } from "/js/header.js";
 
-class News{
+class News {
   //What to put in here?
-  constructor(){
-  }
-  
+  constructor() {}
 
-  getNewsArticles(category){
+  getNewsArticles(category) {
     // Checks if there already is content, if so it removes it
-    const newsSection = document.getElementById("news")
+    const newsSection = document.getElementById("news");
     while (newsSection.firstChild) {
-        newsSection.firstChild.remove()
-    } 
+      newsSection.firstChild.remove();
+    }
 
     fetch(
       `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=${category}`,
-      {
-        method: 'GET',
-        headers: {
-          'x-api-key': 'NbV-VXpvd17m4kTWIYsJN0ofMU326FM_QJwSCwLiR2I'
-        }
-      }
+      header
     )
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
+      .then((res) => res.json()) // parse response as JSON
+      .then((data) => {
+        data.articles.forEach((e, i) => {
+          // Create article element for each article
+          const article = document.createElement("article");
+          article.id = `article${i}`;
+          document.querySelector("#news").appendChild(article);
 
-     data.articles.forEach((e,i) => {
+          // Create and populate headings
+          const heading = document.createElement("a");
+          heading.className = "heading1-news-title";
+          heading.textContent = e.title;
+          heading.href = e.link;
+          document.querySelector(`#article${i}`).appendChild(heading);
 
+          // Create a div for the sources and twitter
+          const divSourceTwitter = document.createElement("div");
+          divSourceTwitter.className = "div-source-twitter";
+          divSourceTwitter.id = `div-source-twitter${i}`;
+          document.querySelector(`#article${i}`).appendChild(divSourceTwitter);
 
-      // Create article element for each article
-      const article = document.createElement('article')
-      article.id = `article${i}`
-      document.querySelector('#news').appendChild(article);
-       
-      
-      // Create and populate headings
-      const heading = document.createElement('a');
-      heading.className = 'heading1-news-title';
-      heading.textContent = e.title;
-      heading.href = e.link
-      document.querySelector(`#article${i}`).appendChild(heading);
+          // Create and populate sources
+          const source = document.createElement("h2");
+          source.className = "heading2-news-source";
+          source.textContent = e.clean_url;
+          document.querySelector(`#div-source-twitter${i}`).appendChild(source);
 
+          // edit the Twitter Account Info if there is Account info
+          if (e.twitter_account !== null && e.twitter_account.startsWith("@")) {
+            const twitterLink = document.createElement("a");
+            twitterLink.id = `twitterLink${i}`;
+            const twitterLinkSrc = e.twitter_account.slice(
+              1,
+              e.twitter_account.length
+            );
+            twitterLink.href = `http://twitter.com/${twitterLinkSrc}`;
+            const twitterLogo = document.createElement("img");
+            twitterLogo.className = "twitter-logo";
+            twitterLogo.src = "assets/twitter.svg";
 
-      // Create a div for the sources and twitter
-      const divSourceTwitter = document.createElement('div')
-      divSourceTwitter.className = 'div-source-twitter'
-      divSourceTwitter.id = `div-source-twitter${i}`
-      document.querySelector(`#article${i}`).appendChild(divSourceTwitter);
+            document
+              .querySelector(`#div-source-twitter${i}`)
+              .appendChild(twitterLink);
+            document.querySelector(`#twitterLink${i}`).appendChild(twitterLogo);
+          }
 
-
-      // Create and populate sources
-      const source = document.createElement('h2');
-      source.className = 'heading2-news-source';
-      source.textContent = e.clean_url;
-      document.querySelector(`#div-source-twitter${i}`).appendChild(source);
-
-
-      // edit the Twitter Account Info if there is Account info
-      if (e.twitter_account !== null && e.twitter_account.startsWith('@')) {
-
-        const twitterLink = document.createElement('a')
-        twitterLink.id = `twitterLink${i}`
-        const twitterLinkSrc = e.twitter_account.slice(1,e.twitter_account.length)
-        twitterLink.href = `http://twitter.com/${twitterLinkSrc}`
-        const twitterLogo = document.createElement('img')
-        twitterLogo.className = 'twitter-logo'
-        twitterLogo.src = 'assets/twitter.svg'
-
-        document.querySelector(`#div-source-twitter${i}`).appendChild(twitterLink)
-        document.querySelector(`#twitterLink${i}`).appendChild(twitterLogo)
-
-      } 
-
-      
-      // Add the divider
-      const divider = document.createElement('div');
-      divider.className = 'news-divider';
-      document.querySelector(`#article${i}`).appendChild(divider);
-     })
-
-
-    })
-    .catch(err => {
-    console.log(`error ${err}`)
-    });
-
+          // Add the divider
+          const divider = document.createElement("div");
+          divider.className = "news-divider";
+          document.querySelector(`#article${i}`).appendChild(divider);
+        });
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
   }
-  
 }
 
 //Create News
-const newHeadlines = new News()
+const newHeadlines = new News();
 
-const buttons = document.querySelector('.links')
-buttons.addEventListener('click', e => {
-  const {target} = e;
-  const {id} = target;
+const buttons = document.querySelector(".links");
+buttons.addEventListener("click", (e) => {
+  const { target } = e;
+  const { id } = target;
   // condition to check user clicks on link
-  if (!target.matches('.link')){
+  if (!target.matches(".link")) {
     return;
   } else {
-    newHeadlines.getNewsArticles(`${id}`)
-  } 
-})
+    newHeadlines.getNewsArticles(`${id}`);
+  }
+});
